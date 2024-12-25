df667cc0bed77299af0e4e943cd33fe5b3ca69c7
raeyoung <raeyoung233@gmail.com>
2024-12-25 22:23:31 +0900

    🎉 프로젝트 초기화

---
+++ src/main/java/com/hhplus/cleanarch/hhplus_clean_architecture/HhplusCleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.hhplus.cleanarch.hhplus_clean_architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusCleanArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhplusCleanArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hhplus/cleanarch/hhplus_clean_architecture/HhplusCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.cleanarch.hhplus_clean_architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusCleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
